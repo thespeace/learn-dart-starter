2d5fab81e9ab377a740a8d3ba6579960d8e38ac0
thespeace <thespeace1@gmail.com>
2024-01-28 19:04:15 +0900

    FEAT: Variables-the var keyword

--- bin/learn_dart_starter.dart
+++ bin/learn_dart_starter.dart
@@ -1,3 +1,8 @@
-void main(){
+void main(){//Entry point
   print('hello world');
+
+  var name = "thespeace";
+  // name = 1; //name 타입을 추측하여 String 타입으로 지정, 정수타입은 업데이트 불가.
+  int age = 20;
+  print("$name, $age");
 }

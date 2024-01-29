8c228fe31d79ba85d39385fc8a4e55ac3d58f6a5
thespeace <thespeace1@gmail.com>
2024-01-29 14:35:42 +0900

    FEAT: Data Types-basic data types

---
+++ bin/data_types.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  /*
+  * -Basic Data Types
+  *  아래 타입을 포함한 거의 대부분의 타입들이 객체(Object)로 이루어져 있다. (함수도 객체)
+  *  이것이 Dart가 진정한 객체 지향 언어로 불리는 이유이다.
+  */
+  String stringVar = "문자열";
+  bool booleanVar = true;
+  int integerVar = 21;
+  double doubleVar = 3.14;
+
+  //num : Integer, Double Class의 부모 Class.
+  num numVar = 12;
+  numVar = 1.1;
+}

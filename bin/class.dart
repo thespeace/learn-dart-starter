30633303b701e1af013499922381208c7fe0ea91
thespeace_notebook <thespeace1@gmail.com>
2024-02-01 15:52:16 +0900

    FEAT: Classes-named constructors

--- bin/class.dart
+++ bin/class.dart
@@ -13,8 +13,19 @@ void main(){
     team: 'red',
     age: 26,
   );
-
   player2.sayHello();
+
+  var redPlayer = Player.createRedPlayer(
+    name: 'kim',
+    age: 22,
+  );
+  redPlayer.sayHello();
+
+  var bluePlayer = Player.createBluePlayer(
+    'lee',
+    22,
+  );
+  bluePlayer.sayHello();
 }
 
 
@@ -22,9 +33,8 @@ void main(){
 /// -Create First Dart Class
 class Player{
   final String name;//Class를 생성할 때는 타입을 꼭 명시해주어야 한다.
-  int xp;
+  int xp, age;
   String team;
-  int age;
 
   Player({
     required this.name,
@@ -33,6 +43,41 @@ class Player{
     required this.age
   }); //constructor method, 동적인 class를 만들 수 있다. 하지만 positional parameters(argument)는 클래스가 커질수록 통제가 어려울 수 있다.
       //따라서 Named Constructor Parameters를 통해서 적절한 제약을 추가해주는 것이 좋다. 여기선 필수값으로 설정했지만 null값일 경우 기본값을 주어서 설정할 수 있다.
+
+
+
+  Player.createRedPlayer({//named syntax
+    required String name,
+    required int age,
+  }) : this.age = age,    //콜론(:)을 사용하면 특별한 생성자 함수를 만들 수 있다.
+       this.name = name,  //콜론을 넣음으로써 dart에게 여기서 객체를 초기화하라고 명령할 수 있다.
+       this.team = 'red',
+       this.xp = 0;
+
+  //하지만 위의 방법은 Named parameters의 일반적인 방법이고 dart는 아래의 간소화된 방법을 추천.
+  Player.createRed({
+    required this.name,
+    required this.age,
+    this.team = 'red',
+    this.xp = 0,
+  });
+
+
+
+  Player.createBluePlayer(String name, int age)//positional syntax
+     : this.age = age,
+       this.name = name,
+       this.team = 'blue',
+       this.xp = 0;
+
+  //Positional parameters 간소화된 방법
+  Player.createBlue(
+      this.name,
+      this.age,
+      [this.team = 'blue', this.xp = 0]
+      );
+
+
 
   void sayHello(){
     print("Hi my name is $name"); //this 생략되어 있고, this를 사용하지 않는 것이 권고되고 있다.(method내에서 같은 이름의 variable가 있어서 어쩔 수 없이 사용하는게 아닌 이상)

a57a94858ce510487d9c516e47b79a6ea45fae46
thespeace_notebook <thespeace1@gmail.com>
2024-02-03 23:08:30 +0900

    FEAT: Classes-abstract classes

--- bin/class2.dart
+++ bin/class2.dart
@@ -1,7 +1,11 @@
 enum Team { red, blue, purple, AOMG } //enum은 개발자가 실수하지 않도록 도와주는 타입으로 선택지를 가진 클래스를 정의할 수 있다.
 enum XPLevel { beginner, medium, pro}
 
-class Player {
+abstract class Human { //추상 클래스는 다른 클래스들이 직접 구현해야 하는 필드와 메소드들을 모아놓은 클래스이다.
+  void walk();         //추상 클래스로 상속받는 클래스들을 강제하기 위해 사용한다.
+}
+
+class Player extends Human{
   String name;
   int xp;
   Team team;
@@ -11,8 +15,17 @@ class Player {
   void sayHello() {
     print("Hi my name is $name, my team is ${team.name}");
   }
+
+  void walk(){
+    print("the player is walking");
+  }
 }
 
+class Coach extends Human {
+  void walk(){
+    print("the coach is walking");
+  }
+}
 
 
 void main() {
@@ -26,6 +39,8 @@ void main() {
   ..name = 'king'
   ..xp = 99999
   ..team = Team.AOMG
-  ..sayHello();
+  ..sayHello()
+  ..walk();
 
+  var coach = Coach()..walk();
 }
